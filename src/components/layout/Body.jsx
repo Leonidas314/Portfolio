90265191e0b4140ef981d410e5015bbda184b6ba
Leon <leondellafiore2016@gmail.com>
2025-12-25 23:06:14 -0300

    Clearing framework, add first base components

---
+++ src/components/layout/Body.jsx
@@ -0,0 +1,8 @@
+export default function Body(){
+    return(
+        <div>
+            <div><p>Project Container</p></div>
+            <div><p>About Me</p></div>
+        </div>
+    );
+}
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,10 @@
+import '../style/Header.css'
+import ProfileImage from './ProfileImage'
+export default function Header(){
+    return(
+    <div className="Header">
+        <p>Welcome to my portfilio</p>  
+        <ProfileImage/>
+    </div>
+    );
+}
