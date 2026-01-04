37344837b6ee3e7bc03abf12cf55fa3f0d0e0fc9
Leon <leondellafiore2016@gmail.com>
2026-01-04 20:46:06 -0300

    Add Skills basic component and css

--- src/components/layout/Body.jsx
+++ src/components/layout/Body.jsx
@@ -1,12 +1,14 @@
 import '../style/Body.css'
 import About from './About';
 import Projects from './Projects';
+import Skills from './Skills';
 export default function Body(){
     return(
         <div className="Body">
             
             <About/>
             <Projects/>
+            <Skills/>
         </div>
     );
 }
---
+++ src/components/layout/Skills.jsx
@@ -0,0 +1,11 @@
+import '../style/Skills.css'
+export default function Skills(){
+    return(
+        <div className="skills-formation">
+            <h2>My Formation and Skills</h2>
+            <div>
+                <p>Place Here the Formation and skills</p>
+            </div>
+        </div>
+    )
+}
