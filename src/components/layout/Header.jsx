b1d3be897d5b4a8cbb0c597998eea9178ee8bd2a
Leon <leondellafiore2016@gmail.com>
2026-01-08 14:45:54 -0300

    Clear Header component from underglow effect

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,56 +1,22 @@
 import '../style/Header.css'
-import { motion, useTransform } from "framer-motion"
-import { useRef, useEffect, useState } from 'react';
-export default function Header({ index, setIndex,scrollProgress }) {
-  const items = ["About", "Projects", "Formation"]
-   const glowX = useTransform(
-    scrollProgress,
-    [0, 1],
-    [0, 2 * 160] // 160px ≈ ancho + gap
-  )
-
-  const buttonRef = useRef(null);
-  const [position, setPosition] = useState({ top: 0, left: 0 });
-
+export default function Header({ index, setIndex }) {
   
-  useEffect(() => {
-    if (buttonRef.current) {
-      const rect = buttonRef.current.getBoundingClientRect();
-      setPosition({ top: rect.top, left: rect.left });
-      // También puedes obtener: rect.right, rect.bottom, rect.width, rect.height
-      console.log('Posición:', rect);
-    }
-  }, [index]); // El array vacío asegura que se ejecute solo una vez al montar
-
+  const items = ["About", "Projects", "Formation"]
+  
   return (
     <header className="header">
       <nav className="nav">
       
         {items.map((label, i) => (
           <button
-            ref = {buttonRef}
             key={label}
             className={`nav-item ${index === i ? "active" : ""}`}
-            onClick={(e) => {
-                  setIndex(i);
-
-                  const rect = e.currentTarget.getBoundingClientRect();
-
-                  console.log({
-                    index: i,
-                    top: rect.top,
-                    left: rect.left,
-                    width: rect.width,
-                    height: rect.height,
-                  });
-                }}          >
+            onClick={(e) => setIndex(i)}
+          >
             {label}
           </button>
         ))}
-        <motion.div
-          className="header-glow"
-          style={{ x: glowX }}
-        />
+        
       </nav>
     </header>
   )
