95ecf4aea9deac588984092e33f68c1437f68f8b
Leon <leondellafiore2016@gmail.com>
2026-01-09 23:08:50 -0300

    Add some temportal properties to Underglow

--- src/components/layout/Underglow.jsx
+++ src/components/layout/Underglow.jsx
@@ -12,7 +12,33 @@ export default function Underglow({scrollProgress}){
   const underlineX = useTransform(
     scrollProgress,
     [0, 1],
-    [0,240] // ejemplo: 3 secciones → 0%, 100%, 200%
+    [125,900] // ejemplo: 3 secciones → 0%, 100%, 200%
+  )
+  const scaleX = useTransform(//visual width variation
+    scrollProgress,
+    [0,1],
+    [0.3,1]
+  )
+
+  const width = useTransform(//structural width variation
+    scrollProgress, 
+    [0, 1], 
+    [40, 240]
+  )
+
+  const opacity = useTransform(//visual opacity variation
+    scrollProgress,
+    [0, 0.2, 1], 
+    [0, 1, 1]
+  )
+
+  const boxShadow = useTransform(//visual glowshadow
+    scrollProgress,
+    [0, 1],
+    [ 
+      "0px 0px 4px rgba(0,255,255,0.2)",
+      "0px 0px 18px rgba(0,255,255,0.9)"
+    ]
   )
 
   return (
