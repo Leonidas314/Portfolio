b20badb6b4b7f7b4a373350b8b68ed116ac3d6f8
Leon <leondellafiore2016@gmail.com>
2026-01-11 19:50:28 -0300

    Redesign About divs, background color

--- src/components/layout/About.jsx
+++ src/components/layout/About.jsx
@@ -3,21 +3,18 @@ import '../style/About.css'
 export default function About() {
     return(
         <div className="about-main-container">
-            
-            <div className="about-content-div" id="about-img-container">
-                <ProfileImage/> 
-
-                <h2>León Dellafiore</h2>
-                <ul>
-                    <li>Computer Science</li>
-                    <li>Software Development</li>
-                </ul>
-            </div>          
-            <div className="about-content-div" id="description">
-                <p>Hi! I'm León.</p><br />
-                <p> Advanced student on computer science,
-                currently on the last instaces of my universitary grade.</p><br />
-                <p>I invite to you to take a look of my portfolio. Greetings! </p>
+            <div id="about-aux-container">            
+                <div id="about-content-div">
+                    <p>Hi! I'm León.</p><br />
+                    <p> Advanced student on computer science,
+                    currently on the last instances of my universitary grade.</p><br />
+                    <p>My academic formation is solid based on data structures, algorithms, relational data bases and software desing patterns. </p><br />
+                    <p>Besides of my academic formation i'm constantly learning about web development technologies like front-end frameworks and clouding services.</p><br/>
+                    <p>I like to focus not only on learning the know how of the technologies, but why to take a particular technical choice.</p>
+                </div>  
+                <div id="about-img-container">  
+                    <ProfileImage/> 
+                </div>
             </div>
         </div>
     )
